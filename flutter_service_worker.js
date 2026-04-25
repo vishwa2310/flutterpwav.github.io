fc5ef76db504d49b10227a84df861472a7963e14
vishwajeet mahajan <30283487+vishwa2310@users.noreply.github.com>
2026-04-25 21:04:28 +0530

    Add files via upload
    new multipal imge

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "c68ffa23ff6785c62722e4f5310d92f9",
+"flutter_bootstrap.js": "73d50c031ae584b618ef3bb54ca97a05",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
